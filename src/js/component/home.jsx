43e33c9cfa73a562e4b84bae60afe2dbc055aa9c
Fiorella-vq <fio.89@hotmail.com>
2024-10-16 16:25:58 -0300

    ejercicio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Todos = () => {
+  const [todos, setTodos] = useState([
+    "Make the bed",
+    "Wash my hands",
+    "Eat",
+    "Walk the dog",
+  ]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <>
+      <h1>Todos</h1>
+      <div className="container mt-5">
+        <div className="card">
+          <div className="card-header">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                value={newTodo}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown} // Detecta la tecla Enter
+                placeholder="What needs to be done?"
+                className="form-control"
+              />
+            </div>
+          </div>
+
+          <ul className="list-group list-group-flush">
+            {todos.length > 0 ? (
+              todos.map((todo, index) => (
+                <li
+                  key={index}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  {todo}
+                  <button
+                    type="button"
+                    className="btn-close btn-li btn-sm"
+                    onClick={() => handleDeleteTodo(index)}
+                  ></button>
+                </li>
+              ))
+            ) : (
+              <li className="list-group-item text-center"></li>
+            )}
+          </ul>
+
+          <div className="card-footer text-muted">
+            {todos.length} item{todos.length !== 1 && "s"} left
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Home;
+export default Todos;
